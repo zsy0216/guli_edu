a76b785a37741b8bf2426cd3833a61d684bd4bf5
zsy0216 <594983498@qq.com>
2020-12-04 22:42:57 +0800

    feat: 配置mybatis plus代码生成器，更新idea版本

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# guli_edu" 
+### 尚硅谷_谷粒学院-2020版微服务+全栈在线教育实战项目
+
+学习地址：[Bilibili](https://www.bilibili.com/video/BV1dQ4y1A75e)
---
+++ guli_service/service_edu/src/test/java/com/tassel/generator/CodeGenerator.java
@@ -0,0 +1,88 @@
+package com.tassel.generator;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import org.junit.Test;
+
+/**
+ * @author ZSY
+ * @since 2018/12/13
+ */
+public class CodeGenerator {
+
+    @Test
+    public void run() {
+
+        // 1、创建代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 2、全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        projectPath = "E:\\Projects\\guli_edu\\guli_service\\service_edu";
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("ZSY");
+        // 生成后是否打开资源管理器
+        gc.setOpen(false);
+        // 重新生成时文件是否覆盖
+        gc.setFileOverride(false);
+        // 去掉Service接口的首字母I UserService
+        gc.setServiceName("%sService");
+        // 主键策略
+        gc.setIdType(IdType.ID_WORKER_STR);
+        // 定义生成的实体类中日期类型
+        gc.setDateType(DateType.ONLY_DATE);
+        // 开启Swagger2模式
+        gc.setSwagger2(true);
+
+        mpg.setGlobalConfig(gc);
+
+        // 3、数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306/guli_edu?serverTimezone=GMT%2B8");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("root");
+        dsc.setDbType(DbType.MYSQL);
+        mpg.setDataSource(dsc);
+
+        // 4、包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.tassel");
+        // 模块名 com.tassel.edu.*
+        pc.setModuleName("edu");
+        pc.setController("controller");
+        pc.setEntity("entity");
+        pc.setService("service");
+        pc.setMapper("mapper");
+        mpg.setPackageInfo(pc);
+
+        // 5、策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        // 要生成的数据库表
+        strategy.setInclude("edu_teacher");
+        // 数据库表映射到实体的命名策略
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        // 生成实体时去掉表前缀
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        // 数据库表字段映射到实体的命名策略
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        // lombok 模型 @Accessors(chain = true) setter链式操作
+        strategy.setEntityLombokModel(true);
+        // restful api风格控制器
+        strategy.setRestControllerStyle(true);
+        // url中驼峰转连字符
+        strategy.setControllerMappingHyphenStyle(true);
+        mpg.setStrategy(strategy);
+
+        // 6、执行
+        mpg.execute();
+    }
+}
